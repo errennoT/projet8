dd2d1a735dbd982b694f55541ffcae8b7814bfa2
John Doe <johndoe@example.com>
2020-02-15 10:28:54 +0100

    add an addflash for task done or not

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -96,7 +96,11 @@ class TaskController extends AbstractController
         $task->toggle(!$task->isDone());
         $this->getDoctrine()->getManager()->flush();
 
-        $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        if ($task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme terminée.', $task->getTitle()));
+        } else {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme non faite.', $task->getTitle()));
+        }
 
         return $this->redirectToRoute('task_list');
     }
